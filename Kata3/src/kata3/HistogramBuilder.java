ee791f5f6c7fc0eb370863d9efedaf36717da26d
usuario <usuario@10.22.143.13>
2014-10-13 14:05:03 +0100

    Override get

---
+++ Kata3/src/kata3/HistogramBuilder.java
@@ -0,0 +1,16 @@
+package kata3;
+
+public class HistogramBuilder<Type> {
+
+    public Histogram<Type> build(Type[] types) {
+        Histogram<Type> histogram = new Histogram();
+        for (Type type : types) {
+            if (histogram.containsKey(type)) {
+                histogram.put(type, histogram.get(type));
+            } else {
+                histogram.put(type, 1);
+            }
+        }
+        return histogram;
+    }
+}
